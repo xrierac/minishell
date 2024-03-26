2ceb0f69c99cfca2884ff50114015e4affeac612
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-03-26 16:50:39 +0200

    Add basic to master

---
+++ inc/minishell.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/12 16:50:37 by xriera-c          #+#    #+#             */
+/*   Updated: 2024/03/26 16:46:18 by xriera-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdlib.h>
+# include <readline.h>
+
+typedef enum e_token_type
+{
+	S_QUOITE, //'
+	D_QUOTE, //'"
+	R_INPUT, //<
+	R_OUTPUT, //>
+	HEREDOC, //<<
+	APPEND, //>>
+	PIPE, //|
+	ENV, //$
+	STATUS, //$?
+	CMD,
+	ARG
+}	t_token_type;
+
+#endif
