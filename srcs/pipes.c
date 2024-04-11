3acbcf65c62ec1fafa79e484a9b43cd777225a60
Timothy Campbell <tcampbel@c2r5p10.hive.fi>
2024-04-11 17:13:33 +0300

    replaced loo with find_quotes function

--- srcs/pipes.c
+++ srcs/pipes.c
@@ -6,7 +6,7 @@
 /*   By: tcampbel <tcampbel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 12:59:28 by tcampbel          #+#    #+#             */
-/*   Updated: 2024/04/09 15:22:05 by tcampbel         ###   ########.fr       */
+/*   Updated: 2024/04/11 14:24:14 by tcampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,37 +17,23 @@
 // 	ft_strrchr(input, q);
 // }
 
-void count_pipes(t_sh *msh, char *input)
+void	count_pipes(t_sh *msh, char *input)
 {
-	int	d_quote;
-	int	s_quote;
 	int	i;
 
 	i = -1;
-	d_quote = 0;
-	s_quote = 0;
 	msh->pipes = 0;
 	while (input[++i])
 	{
-		if (input[i] == '|')
+		if (input[i] == '\'' || input[i] == '\"')
+			i = find_quote(input, input[i], i);
+		if (input[0] == '|' || (input[i] == '|' && input[i + 1] == '|') \
+			|| (input[i] == '|' && input[i + 1] == '\0'))
+		{
+			ft_printf(2, ":): syntax error near unexpected token `|'\n");
+			return ;
+		}
+		else if (input[i] == '|')
 			msh->pipes++;
-		if (input[i] == '\'')
-		{
-			++i;
-			while (input[i] != '\'' && input[i])
-				++i;
-			s_quote++;
-		}
-		if (input[i] == '\"')
-		{
-			++i;
-			while (input[i] != '\"' && input[i])
-				++i;
-			d_quote++;
-		}
 	}
-	if (s_quote % 2 != 0)
-		printf("quote>\n");
-	else if (d_quote % 2 != 0)
-		printf("dquote>\n");
 }
