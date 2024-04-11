e1fcb98c41a892dca68803442be2cb111fac7d62
Timothy Campbell <tcampbel@c2r5p10.hive.fi>
2024-04-11 17:10:52 +0300

    added get_token function, needs to be linked with format_str

--- srcs/tokens.c
+++ srcs/tokens.c
@@ -6,7 +6,7 @@
 /*   By: tcampbel <tcampbel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 13:14:34 by tcampbel          #+#    #+#             */
-/*   Updated: 2024/04/09 18:06:28 by tcampbel         ###   ########.fr       */
+/*   Updated: 2024/04/11 17:05:32 by tcampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,30 +22,53 @@ t_bool	ft_isspace(char str)
 t_bool	is_op(char *str, int i)
 {
 	if ((str[i] == '<' && str[i + 1] == '<') \
-		|| (str[i] == '>' && str[i + 1] == '>'))
+		|| (str[i] == '>' && str[i + 1] == '>') \
+		|| (str[i] == '$' && str[i + 1] == '?'))
 		return (true);
-	else if (str[i] == '|' || str[i] == '<' || str[i] == '>' || str[i] == '$')
+	else if (str[i] == '|' || str[i] == '<' || str[i] == '>')
 		return (true);
 	return (false);
 }
 
-void	is_cmd(t_sh *msh, char *str)
+// t_bool	is_cmd(t_sh *msh, char *str)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	if (!str)
+// 		return (false);
+// 	while (ft_isspace(str[i]))
+// 		i++;
+// 	if (access(str, F_OK) == 0)
+// 		return (true);
+// 	else if (is_builtin(msh, str))
+// 		return (true);
+// 	return (false);
+// }
+
+t_bool	is_arg(t_sh *msh, char *str)
 {
 	int	i;
 
 	i = -1;
+	if (!str)
+		return (false);
 	while (str[++i])
 	{
-		while (ft_isspace(str[i] && str[i]) && (!is_op(str, i)))
-			++i;
-		if ((!is_op(str, i)) && str[i] != '\0')
+		if (str[i] == '\'')
 		{
-			while (str[i] && (!(is_op(str, i))) && (!ft_isspace(str[i])))
+			++i;
+			while (str[i] && str[i] != '\'')
 				++i;
-			msh->tok_count++;
+			if (str[i] == '\'')
+				return (true);
 		}
 	}
+	return (false);
 }
+
+// Probably don't need this anymore, was made to check how many tokens are 
+// needed to malloc but now splitting with pipes
 
 void	count_tokens(char *input, t_sh *msh)
 {
@@ -59,33 +82,37 @@ void	count_tokens(char *input, t_sh *msh)
 			msh->tok_count++;
 	}
 	//is_cmd(msh, input);
-	printf("%i\n", msh->tok_count);
+	//printf("%i\n", msh->tok_count);
 }
 
 void	lexer(char *input, t_sh *msh)
 {
-	int	i;
-	int	j;
+	int		i;
+	char	**tok_check;
 
 	i = 0;
-	j = msh->len;
-	while (j > 0)
+	tok_check = ft_split(input, ' ');
+	if (!tok_check)
 	{
-		msh->lex_arr[i]->tok_arr = ft_split(input, '|');
-		if (!msh->lex_arr[i]->tok_arr)
-			printf("malloc error\n");
-		i++;
-		j--;
+		free_all(msh);
+		exit_error(msh, "ft_split\n", 127);
 	}
-	i = 0;
-	while (msh->lex_arr[i])
-	{
-		
-		while (msh->lex_arr[i]->tok_arr[j])
-		{
-			printf("%s\n", msh->lex_arr[i]->tok_arr[j]);
-			j++;
-		}
-		i++;
-	}
+}
+
+void	get_token(t_sh *msh, t_lex *lex)
+{
+	if (ft_strncmp(lex->tok_arr, "$", 2)
+		lex->token = ENV;
+	else if (ft_strncmp(lex->tok_arr, "$?", 3)
+		lex->token = STATUS;
+	else if (ft_strncmp(lex->tok_arr, "<", 2)
+		lex->token = R_INPUT;
+	else if (ft_strncmp(lex->tok_arr, ">", 2)
+		lex->token = R_OUTPUT;
+	else if (ft_strncmp(lex->tok_arr, "<<", 3)
+		lex->token = HEREDOC;
+	else if (ft_strncmp(lex->tok_arr, ">>", 3)
+		lex->token = APPEND;
+	else
+		lex->token = CMD;
 }
