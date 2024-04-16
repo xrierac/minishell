1710ce05d11e08b1f1f6850e9bf67f09073b75b9
Timothy Campbell <tcampbel@c3r4p4.hive.fi>
2024-04-16 17:51:19 +0300

    Minor changes -> changed iterator to reset after triple pointer is calloced

--- srcs/initialise.c
+++ srcs/initialise.c
@@ -6,25 +6,26 @@
 /*   By: tcampbel <tcampbel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 13:18:50 by tcampbel          #+#    #+#             */
-/*   Updated: 2024/04/09 18:02:54 by tcampbel         ###   ########.fr       */
+/*   Updated: 2024/04/16 14:12:04 by tcampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-t_sh	*initialise(char **ev)
+t_sh	*init_msh(char **ev)
 {
 	t_sh	*msh;
 
 	msh = ft_calloc(1, sizeof(t_sh));
 	if (!msh)
-		exit_error(msh, "calloc: ", 127);
+		exit_error(msh, "calloc\n", 127);
 	msh->env = ft_calloc(1, sizeof(t_env));
 	if (!msh->env)
 	{
 		free_all(msh);
-		exit_error(msh, "calloc: ", 127);
+		exit_error(msh, "calloc\n", 127);
 	}
+	init_env(msh->env);
 	msh->pipes = 0;
 	msh->tok_count = 0;
 	ft_envcpy(msh, msh->env, ev);
@@ -38,21 +39,22 @@ void	init_env(t_env *env)
 	env->path_arr = NULL;
 }
 
-t_lex **init_lex(t_sh *msh)
+t_lex	***init_lex(t_sh *msh)
 {
-	int i;
+	int	i;
 
 	msh->len = msh->pipes + 1;
 	i = -1;
-	msh->lex_arr = ft_calloc(msh->len + 1, sizeof(t_lex *));
+	msh->lex_arr = (t_lex ***)ft_calloc(msh->len + 1, sizeof(t_lex **));
 	if (!msh->lex_arr)
 	{
 		free_all(msh);
 		exit_error(msh, "ft_calloc\n", 127);
 	}
+	i = -1;
 	while (++i < msh->len)
 	{
-		msh->lex_arr[i] = ft_calloc(1, sizeof(t_lex));
+		msh->lex_arr[i] = (t_lex **)ft_calloc(1, sizeof(t_lex *));
 		if (!msh->lex_arr[i])
 		{
 			free_all(msh);
