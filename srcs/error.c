19467ffa6025b99ecd6e7c3105568a1fa1ce4cf5
Timothy Campbell <tcampbel@c1r1p3.hive.fi>
2024-04-19 18:23:57 +0300

    Updated color definition

--- srcs/error.c
+++ srcs/error.c
@@ -6,7 +6,7 @@
 /*   By: tcampbel <tcampbel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 13:45:08 by tcampbel          #+#    #+#             */
-/*   Updated: 2024/04/05 13:46:08 by tcampbel         ###   ########.fr       */
+/*   Updated: 2024/04/18 10:39:00 by tcampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	exit_error(t_sh *msh, char *msg, int status)
 	if (msh)
 	{
 		free_all(msh);
-		ft_printf(2, HRED":( %s\n"RESET, msg);
+		ft_printf(2, RED":( %s\n"END, msg);
 	}
 	exit(status);
 }
