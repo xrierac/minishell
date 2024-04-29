0dbb2c8f0a05557fb0ec979c95e823dc88d85437
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-04-29 14:18:26 +0300

    Add r_output

--- src/execution/redirect.c
+++ src/execution/redirect.c
@@ -6,19 +6,31 @@
 /*   By: xriera-c <xriera-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 16:48:01 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/04/29 11:54:09 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/04/29 14:18:02 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-void	r_input(t_lex *file)
+void	r_input(char **cmd_arr)
 {
 	int	infile;
 
-	infile = open(file->cmd_arr[0], O_RDONLY, 0444);
+	infile = open(cmd_arr[0], O_RDONLY, 0444);
 	if (infile == -1)
 		exit(0);
 	if (dup2(infile, STDIN_FILENO) == -1)
 		exit(0);
+}
+
+void	r_output(char **cmd_arr)
+{
+	int	outfile;
+
+	outfile = open(cmd_arr[0], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	if (outfile == -1)
+		exit(0);
+	if (dup2(outfile, STDOUT_FILENO) == -1)
+		exit(0);
+	
 }
