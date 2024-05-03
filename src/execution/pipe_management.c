dc49064c1a70e475a3589be578295cdda6a3bb8a
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-05-03 11:13:16 +0300

    Add pipe_management.c

---
+++ src/execution/pipe_management.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipe_management.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xriera-c <xriera-c@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/03 10:13:51 by xriera-c          #+#    #+#             */
+/*   Updated: 2024/05/03 10:42:07 by xriera-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/minishell.h"
+
+int pipe_management(t_sh *sh_data, int index, int **pipefd)
+{
+    if (sh_data->len == 1)
+        return 0;
+    if (index > 0 && index <= sh_data->len - 1)
+        if (dup2(pipefd[index - 1][0], STDIN_FILENO) == -1)
+            return (1);
+    if (index < sh_data->len - 1)
+        if (dup2(pipefd[index][1], STDOUT_FILENO) == -1)
+            return (1);
+    return (0);   
+}
