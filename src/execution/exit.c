41310f0c31fcc94dd483f810f70124e90df1ad96
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-05-06 12:29:50 +0300

    Change name to exit.c

--- src/execution/exit.c
+++ src/execution/exit.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   error_handling.c                                   :+:      :+:    :+:   */
+/*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: xriera-c <xriera-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 15:33:02 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/05/03 16:48:01 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/05/06 09:51:45 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,9 @@ void	error_cmd_not_found(char *str)
 	ft_putstr_fd("minishell: ", 2);
 	ft_putstr_fd(str, 2);
 	ft_putstr_fd(": command not found\n", 2);
+}
+
+void	good_exit(char *str)
+{
+	
 }
