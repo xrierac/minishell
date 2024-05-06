f31a5e25612ca18f72814ad18e56b98412ba2de4
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-05-06 12:28:27 +0300

    Add builtin_check function

---
+++ src/builtins/builtin_check.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_check.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xriera-c <xriera-c@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/06 11:48:20 by xriera-c          #+#    #+#             */
+/*   Updated: 2024/05/06 12:11:45 by xriera-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/minishell.h"
+
+int	builtin_check(t_lex *lex, t_env *env)
+{	
+	if (!ft_strncmp(lex->cmd_arr[0], "echo", ft_strlen(lex->cmd_arr[0])))
+		return (ft_echo(lex->cmd_arr, env->env_arr));
+	if (!ft_strncmp(lex->cmd_arr[0],"cd", ft_strlen(lex->cmd_arr[0])))
+		return (ft_cd(lex->cmd_arr[1], env->env_arr));
+	if (!ft_strncmp(lex->cmd_arr[0], "pwd", ft_strlen(lex->cmd_arr[0])))
+		return (ft_pwd());
+	if (!ft_strncmp(lex->cmd_arr[0],"env", ft_strlen(lex->cmd_arr[0])))
+		return (ft_env(env->env_arr));
+	//if (!ft_strncmp(lex->cmd_arr[0], "export", ft_strlen(lex->cmd_arr[0])))
+	//	return (ft_export(lex->cmd_arr));
+	return (1);
+}
