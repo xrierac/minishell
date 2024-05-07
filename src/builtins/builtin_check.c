5c5c8255de7869a1d0b05754b5679610bcd5207c
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-05-07 11:12:37 +0300

    Add ft_export

--- src/builtins/builtin_check.c
+++ src/builtins/builtin_check.c
@@ -6,7 +6,7 @@
 /*   By: xriera-c <xriera-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 11:48:20 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/05/06 15:47:20 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/05/07 11:07:31 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,9 @@
 int	parent_builtin(t_lex *lex, t_env *env)
 {
 	if (!ft_strncmp(lex->cmd_arr[0],"cd", ft_strlen(lex->cmd_arr[0])))
-		return (ft_cd(lex->cmd_arr[1], env->env_arr));
+		return (ft_cd(lex->cmd_arr[1], env));
+	if (!ft_strncmp(lex->cmd_arr[0], "export", ft_strlen(lex->cmd_arr[0])))
+		return (ft_export(env, lex->cmd_arr[1]));
 	return (1);
 }
 
@@ -27,7 +29,5 @@ int	builtin_check(t_lex *lex, t_env *env)
 		return (ft_pwd());
 	if (!ft_strncmp(lex->cmd_arr[0],"env", ft_strlen(lex->cmd_arr[0])))
 		return (ft_env(env->env_arr));
-	//if (!ft_strncmp(lex->cmd_arr[0], "export", ft_strlen(lex->cmd_arr[0])))
-	//	return (ft_export(lex->cmd_arr));
 	return (1);
 }
