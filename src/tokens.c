bbb2fbd78dadc0d4cd313bc0bee8a91b48ead2be
tcampbel <tcampbel22@gmail.com>
2024-05-09 17:42:57 +0300

    Reworked tok_struct function to new memoy allocation structure

--- src/tokens.c
+++ src/tokens.c
@@ -23,7 +23,7 @@ void	is_token(t_sh *msh, char *str)
 			i++;
 		else if ((str[i] == '<' && str[i + 1] == '<') \
 			|| (str[i] == '>' && str[i + 1] == '>') \
-			|| (str[i] == '!' && str[i + 1] == '?'))
+			|| (str[i] == '$' && str[i + 1] == '?'))
 		{
 			msh->tok_count++;
 			i += 2;
@@ -50,19 +50,17 @@ void	create_tok_struct(t_sh *msh)
 	int		i;
 
 	i = -1;
-	while (++i < msh->len)
+	while (++i < msh->processes)
 	{
 		msh->tok_count = 0;
 		msh->pipe_arr[i] = ft_strtrim(msh->pipe_arr[i], " ");
 		if (!msh->pipe_arr[i])
-		{
-			free_all(msh);
 			exit_error(msh, "ft_strtrim\n", 127);
-		}
 		is_token(msh, msh->pipe_arr[i]);
 		init_token(msh, msh->lex_arr[i]);
 		assign_token(msh, msh->lex_arr[i], msh->pipe_arr[i]);
 	}
+	ft_free_array(msh->pipe_arr);
 }
 
 
@@ -75,11 +73,11 @@ void	assign_token(t_sh *msh, t_lex **lex_arr, char *cmd)
 	temp = NULL;
 	i = -1;
 	j = 0;
-	while (++i <= msh->tok_count)
+	while (++i < msh->tok_count)
 	{
-		lex_arr[i]->cmd_arr = init_cmd_arr(msh);
 		if (cmd[j] == '<' && cmd[j + 1] != '<')
 		{
+			lex_arr[i]->cmd_arr = init_cmd_arr(msh);
 			lex_arr[i]->token = R_INPUT;
 			j += 1;
 			lex_arr[i]->cmd_arr[0] = ft_substr(cmd, j, find_space(cmd, j));
@@ -87,6 +85,7 @@ void	assign_token(t_sh *msh, t_lex **lex_arr, char *cmd)
 		}
 		else if (cmd[j] == '>' && cmd[j + 1] != '>')
 		{
+			lex_arr[i]->cmd_arr = init_cmd_arr(msh);
 			lex_arr[i]->token = R_OUTPUT;
 			j += 1;
 			lex_arr[i]->cmd_arr[0] = ft_substr(cmd, j, find_space(cmd, j));
@@ -94,13 +93,15 @@ void	assign_token(t_sh *msh, t_lex **lex_arr, char *cmd)
 		}
 		else if (cmd[j] == '>' && cmd[j + 1] == '>')
 		{
+			lex_arr[i]->cmd_arr = init_cmd_arr(msh);
 			lex_arr[i]->token = APPEND;
 			j += 2;
 			lex_arr[i]->cmd_arr[0] = ft_substr(cmd, j, find_space(cmd, j));
 			j = find_space(cmd , j);
 		}
-		else if (cmd[i] == '<' && cmd[i + 1] == '<')
+		else if (cmd[j] == '<' && cmd[j + 1] == '<')
 		{
+			lex_arr[i]->cmd_arr = init_cmd_arr(msh);
 			lex_arr[i]->token = HEREDOC;
 			j += 2;
 			lex_arr[i]->cmd_arr[0] = ft_substr(cmd, j, find_space(cmd, j));
@@ -108,19 +109,14 @@ void	assign_token(t_sh *msh, t_lex **lex_arr, char *cmd)
 		}
 		else if (is_op(cmd, j) == false)
 		{
-			lex_arr[i]->token = CMD;
 			temp = ft_substr(cmd, j, find_op(cmd, j));
 			j = find_op(cmd, j);
 			lex_arr[i]->cmd_arr = ft_split(temp, ' ');
 			free(temp);
-			if (!lex_arr[i])
-			{
-				free_all(msh);
+			if (!lex_arr)
 				exit_error(msh, "ft_split", 127);
-			}
+			lex_arr[i]->token = CMD;
 		}
-		else
-			lex_arr[i]->token = NOT_DEF;
 	}
 }
 
@@ -132,10 +128,7 @@ void	lexer(char *input, t_sh *msh)
 	msh->lex_arr = init_lex(msh);
 	msh->pipe_arr = ft_strtok(input, '|', "'\'''\"'");
 	if (!msh->pipe_arr)
-	{
-		free_all(msh);
 		exit_error(msh, "ft_strtok\n", 127);
-	}
 	create_tok_struct(msh);
-	free(input);
+	//free(input);
 }
