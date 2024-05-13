682806383ae3ab0677f32858ca53b34887b587cf
Timothy Campbell <tcampbel@c3r4p4.hive.fi>
2024-05-13 18:10:51 +0300

    Condensed assign_token function to 25 lines, moved extra functions to tokens_two, deleted tokens_three

--- src/tokens_two.c
+++ src/tokens_two.c
@@ -6,35 +6,63 @@
 /*   By: tcampbel <tcampbel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 14:44:06 by tcampbel          #+#    #+#             */
-/*   Updated: 2024/04/29 14:24:04 by tcampbel         ###   ########.fr       */
+/*   Updated: 2024/05/13 17:57:16 by tcampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-char	*redirect_in(t_sh *msh, char *input)
+void	token_type(t_sh *msh, t_lex *lex, char *cmd, int j)
+{
+	if (cmd[j] == '<' && cmd[j + 1] != '<')
+		lex->token = R_INPUT;
+	else if (cmd[j] == '>' && cmd[j + 1] != '>')
+		lex->token = R_OUTPUT;
+	else if (cmd[j] == '>' && cmd[j + 1] == '>')
+		lex->token = APPEND;
+	else if (cmd[j] == '<' && cmd[j + 1] == '<')
+		lex->token = HEREDOC;
+}
+
+int	tokenise_cmd(t_sh *msh, t_lex *lex, char *cmd, int j)
 {
 	int		i;
 	char	*temp;
 
-	i = 0;
-	temp = ft_substr(input, i, 1);
-	return (temp);
+	temp = NULL;
+	i = -1;
+	temp = ft_substr(cmd, j, find_op(cmd, j));
+	j = find_op(cmd, j);
+	lex->cmd_arr = ft_strtok(temp, ' ', "\'\"");
+	free(temp);
+	if (!lex->cmd_arr)
+		exit_error(msh, "ft_strtok", 127);
+	while (lex->cmd_arr[++i])
+	{
+		lex->cmd_arr[i] = ft_strtrim(lex->cmd_arr[i], " ");
+		lex->cmd_arr[i] = remove_quotes(msh, lex->cmd_arr[i]);
+	}
+	lex->token = CMD;
+	return (j);
 }
 
-void	get_token(t_sh *msh, t_lex *lex_arr, char *str)
+int	tokenise_op(t_sh *msh, t_lex  *lex, char *cmd, int j)
 {
-	if (ft_strncmp(str, "$?", 3))
-		lex_arr->token = STATUS;
-	else if (ft_strncmp(str, "<", 2))
-		lex_arr->token = R_INPUT;
-	else if (ft_strncmp(str, ">", 2))
-		lex_arr->token = R_OUTPUT;
-	else if (ft_strncmp(str, "<<", 3))
-		lex_arr->token = HEREDOC;
-	else if (ft_strncmp(str, ">>", 3))
-		lex_arr->token = APPEND;
-	// else if (first_str(str) == true)
-	// 	lex_arr->token = CMD;
-}
+	char	*temp;
 
+	temp = NULL;
+	lex->cmd_arr = init_cmd_arr(msh);
+	token_type(msh, lex, cmd, j);
+	if (lex->token == R_INPUT || lex->token == R_OUTPUT)
+		j++;
+	else if (lex->token == APPEND || lex->token == HEREDOC)
+		j += 2;
+	temp = ft_substr(cmd, j, find_space(cmd, j));
+	if (!temp)
+		exit_error(msh, "ft_substr", 127);
+	lex->cmd_arr[0] = ft_strtrim(temp, " ");
+	if (!lex->cmd_arr[0])
+		exit_error(msh, "ft_strtrim", 127);
+	j = find_space(cmd, j);
+	return (j);
+}
