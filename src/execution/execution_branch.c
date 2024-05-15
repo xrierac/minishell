990bfd33ad89f8f0eb1c62c84c5b84ac08b083b9
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-05-15 14:40:19 +0300

    Basic improvements

--- src/execution/execution_branch.c
+++ src/execution/execution_branch.c
@@ -6,7 +6,7 @@
 /*   By: xriera-c <xriera-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 10:46:29 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/05/14 14:56:57 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/05/15 14:13:35 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,30 +56,52 @@ static void	child_start(t_sh *sh_data, int index, int pipefd[][2])
 		error_cmd_not_found(sh_data->lex_arr[index][0]->cmd_arr[0]);
 }
 
+static int	open_processes(t_sh *sh_data, int pipefd[][2])
+{
+	pid_t	cpid[MAX_FD];
+	int		i;
+
+	i = -1;
+	while (++i < sh_data->processes)
+	{
+		cpid[i] = fork();
+		if (cpid[i] < 0)
+		{
+			wait_processes(cpid, i);
+			return (generic_error("", "fork"));
+		}
+		if (cpid[i] == 0)
+		{
+			child_start(sh_data, i, pipefd);
+			exit(0);
+		}
+	}
+	return (wait_processes(cpid, i));
+}
+
 int	execution_branch(t_sh *sh_data)
 {
 	int		i;
 	int		pipefd[MAX_FD][2];
-	pid_t	cpid[MAX_FD];
+	int		val;
 
+	i = run_builtin(sh_data, sh_data->lex_arr[0][0]->cmd_arr);
+	if (i >= 0)
+		return (i);
 	i = -1;
 	while (++i < sh_data->pipes)
-		if (pipe(pipefd[i]) == -1)
-			exit(0);
-	if (run_builtin(sh_data, sh_data->lex_arr[0][0]->cmd_arr) >= 0)
-		return (close_pipes(pipefd, i, sh_data), 0);
-	i = -1;
-	while (++i < sh_data->processes)
 	{
-			cpid[i] = fork();
-			if (cpid[i] < 0)
-				exit(0);
-			if (cpid[i] == 0)
+		if (pipe(pipefd[i]) == -1)
+		{
+			while (--i > -1)
 			{
-				child_start(sh_data, i, pipefd);
-				exit(0);
+				close(pipefd[i][0]);
+				close(pipefd[i][1]);
 			}
+			return (generic_error("", "pipe"));
+		}
 	}
+	val = open_processes(sh_data, pipefd);
 	close_pipes(pipefd, i, sh_data);
-	return (wait_processes(cpid, i));
+	return (val);
 }
