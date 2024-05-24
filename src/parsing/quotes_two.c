1d117db135ae8765f1d11784aa3ed37d8bbc3e5b
Timothy Campbell <tcampbel@c2r3p8.hive.fi>
2024-05-24 16:23:40 +0300

    Removed printf's, added extra iterator to remove quotes function

--- src/parsing/quotes_two.c
+++ src/parsing/quotes_two.c
@@ -6,7 +6,7 @@
 /*   By: tcampbel <tcampbel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 16:02:56 by tcampbel          #+#    #+#             */
-/*   Updated: 2024/05/22 16:03:37 by tcampbel         ###   ########.fr       */
+/*   Updated: 2024/05/24 15:49:58 by tcampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,37 @@ int	skip_quotes(char *str, int i)
 	if (str[i] == '\'' || str[i] == '\"')
 		i = find_quote(str, str[i], i + 1);
 	return (i);
+}
+
+char	*handle_squote(t_sh *msh, char *ptr)
+{
+	if (*ptr == '\'')
+		msh->buffer[msh->buf_len++] = *ptr++;
+	while (*ptr != '\'' && *ptr)
+		msh->buffer[msh->buf_len++] = *ptr++;
+	if (*ptr == '\'')
+		msh->buffer[msh->buf_len++] = *ptr++;
+	return (ptr);
+}
+
+char	*handle_dquote(t_sh *msh, char *ptr)
+{
+	if (*ptr == '\"')
+		msh->buffer[msh->buf_len++] = *ptr++;
+	while (*ptr != '\"' && *ptr)
+	{
+		if (*ptr == '$' && (*(ptr + 1) != ' ' && *(ptr + 1)) != '\0' \
+			&& *(ptr + 1) != '\"')
+		{
+			ptr = deref_var(msh, ptr + 1);
+			if (msh->var[0] == '\0' && *ptr == '\0')
+				break ;
+		}
+		else
+			msh->buffer[msh->buf_len++] = *ptr++;
+	}
+	if (*ptr == '\"')
+		msh->buffer[msh->buf_len++] = *ptr++;
+	msh->buffer[msh->buf_len] = '\0';
+	return (ptr);
 }
