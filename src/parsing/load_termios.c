c2290178bf5b0c0c9fe00f092f4b9d89abb34c1a
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-05-30 17:14:59 +0300

    Add tcsetreadline function

--- src/parsing/load_termios.c
+++ src/parsing/load_termios.c
@@ -6,11 +6,24 @@
 /*   By: xriera-c <xriera-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 15:05:14 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/05/29 15:14:04 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/05/30 15:42:08 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
+
+char	*tcsetreadline(t_sh *msh, int n)
+{
+	char *input;
+
+	tcsetattr(STDIN_FILENO, 0, &msh->new);
+	if (n == 0)
+		input = readline("MiNiH3LL> ");
+	else
+		input = readline("> ");
+	tcsetattr(STDIN_FILENO, 0, &msh->old);
+	return (input);
+}
 
 void	load_termios(t_sh *msh)
 {
