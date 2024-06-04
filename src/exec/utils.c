743afbc4e8f5f546b0a0f8f5c443014fc81279ad
xriera-c <xriera-c@student.hive.fi>
2024-06-04 20:27:36 +0300

    Solve small issue with PATH=

--- src/exec/utils.c
+++ src/exec/utils.c
@@ -6,7 +6,7 @@
 /*   By: xriera-c <xriera-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 09:54:17 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/05/24 15:34:21 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/06/04 19:39:37 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ char	*get_name(char *str)
 
 int	new_path_arr(t_env *env_s, char *str)
 {
-	if (ft_strncmp(str, "PATH", 4) == 0 && ft_strlen(str) == 4)
+	if (ft_strncmp(str, "PATH=", 5) == 0)
 	{
 		ft_free_array(env_s->path_arr);
 		env_s->path_arr = ft_split(ft_getenv("PATH=", env_s->env_arr), ':');
