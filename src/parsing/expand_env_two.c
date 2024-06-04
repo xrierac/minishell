a07a5b43724007dd41b372ee253d50afbe4bb4cc
Timothy Campbell <tcampbel@c1r5p17.hive.fi>
2024-06-04 14:00:42 +0300

    Refactored and split into multile functions, set variables to NULL after freeing

--- src/parsing/expand_env_two.c
+++ src/parsing/expand_env_two.c
@@ -6,11 +6,45 @@
 /*   By: tcampbel <tcampbel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 15:01:05 by tcampbel          #+#    #+#             */
-/*   Updated: 2024/05/30 17:08:09 by tcampbel         ###   ########.fr       */
+/*   Updated: 2024/06/04 13:23:10 by tcampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
+
+void	handle_expansion(t_sh *msh, char *cmd)
+{
+	char	*ptr;
+
+	ptr = cmd;
+	while (*ptr)
+	{
+		if (*ptr == '\'')
+			ptr = handle_squote(msh, ptr);
+		else if (*ptr == '\"')
+			ptr = handle_dquote(msh, ptr);
+		else if (*ptr == '$' && ft_isspace(*(ptr + 1)) == 0 \
+					&& *(ptr + 1) != '\0' && *(ptr + 1) != '$')
+		{
+			ptr = deref_var(msh, ptr + 1);
+			if (msh->var == NULL && *ptr == '\0')
+				break ;
+		}
+		else
+			msh->buffer[msh->buf_len++] = *ptr++;
+	}
+	msh->buffer[msh->buf_len] = '\0';
+}
+
+char	*env_var_type_check(char *ptr)
+{
+	if (*ptr == '?' || ft_isdigit(*ptr) == 1)
+		ptr++;
+	else
+		while (*ptr && *ptr != '$' && ((ft_isalnum(*ptr) == 1) || *ptr == '_'))
+			ptr++;
+	return (ptr);
+}
 
 char	*fetch_exit_code(t_sh *msh, char *ptr)
 {
