04f5f90c741b029189289d62f7feb7782ac7d515
Timothy Campbell <tcampbel@c1r4p11.hive.fi>
2024-06-06 15:40:02 +0300

    Fixed PWD when unset, norm checks and empty line leaks

--- src/parsing/parse_env_two.c
+++ src/parsing/parse_env_two.c
@@ -6,29 +6,26 @@
 /*   By: tcampbel <tcampbel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 11:03:47 by tcampbel          #+#    #+#             */
-/*   Updated: 2024/06/06 13:14:40 by tcampbel         ###   ########.fr       */
+/*   Updated: 2024/06/06 15:37:26 by tcampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-void	get_pwd(t_sh *msh, char **temp, int i)
+void	new_pwd(t_sh *msh, t_env *env_s)
 {
-	char	*pwd;
+	char 	*str;
+	char	**arr;
 
-	pwd = return_ptr_cwd(msh->env);
-	if (!pwd)
+	str = ft_strjoin("export PWD=", return_ptr_cwd(env_s));
+	if (!str)
 		exit_error(msh, "malloc", 2);
-	free(temp[i]);
-	temp[i] = ft_strjoin("PWD=", pwd);
-	if (!temp[i])
-	{
-		free(pwd);
-		pwd = NULL;
+	arr = ft_split(str, ' ');
+	free(str);
+	if (!arr)
 		exit_error(msh, "malloc", 2);
-	}
-	free(pwd);
-	pwd = NULL;
+	ft_export(env_s, arr, 0);
+	ft_free_array(arr);
 }
 
 void	get_home(t_sh *msh, char **env)
